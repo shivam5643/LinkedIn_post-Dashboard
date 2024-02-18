c43ef2bfcf2e8976040faf9ee51d9d46f47ec7c5
shivam <shivam22331100@gmail.com>
2024-02-18 11:32:29 +0530

    updated

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -10,19 +10,19 @@ const ContactPage = ({ open, setOpen }) => {
 
     return (
         <Dialog open={open} onClose={handleClose}>
-            <div className="max-w-lg mx-auto bg-white rounded-lg shadow-md p-6">
+            <div className=" mx-auto bg-white rounded-lg shadow-md p-6 max-h-[800px] py-2">
                 {formData && (
                     <div>
                         <h3 className="text-3xl font-bold mb-4  text-gray-800">{formData.title}</h3>
-                        <p className="text-lg text-gray-700 mb-4 font-montserrat">{formData.disclaimer}</p>
-                        <p className="text-xl text-gray-800 mb-6 font-palanquin font-semibold">{formData.description}</p>
-                        <p className="text-lg text-gray-800 mb-4">{formData.instruction}</p>
+                        <p className="text-lg text-gray-700 mb-3 font-montserrat">{formData.disclaimer}</p>
+                        <p className="text-xl text-gray-800 mb-3 font-palanquin font-semibold">{formData.description}</p>
+                        <p className="text-lg text-gray-800 ">{formData.instruction}</p>
                         <div className="flex flex-wrap mb-4">
                             {formData.hashtags && formData.hashtags.split(',').map((tag, index) => (
                                 <span key={index} className="mr-2 mb-2 text-blue-500">#{tag.trim()}</span>
                             ))}
                         </div>
-                        <img src={formData.image} alt="Post Image" className="w-full mb-4 rounded-lg shadow-md" />
+                        <img src={formData.image} alt="Post Image" className="w-[500px] ml-4 mb-4 rounded-lg shadow-md h-[300px]" />
                     </div>
                 )}
             </div>
